1fe7e6d56a38a6df06a279318a3b5b0dd598cd1d
soyeong <soyeong9712@gmail.com>
2025-12-19 12:27:23 +0900

    fix db port

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ class Database:
             self.connection = pymysql.connect(
                 host='mariadb',
                 # host='svc.sel5.cloudtype.app',
-                # port=3306,
+                port=3306,
                 database='test',  # test 데이터베이스 사용
                 user='root',
                 password='a123',  # mariadb 설치 당시의 패스워드, 실제 환경에서는 보안을 위해 환경변수 등을 사용
